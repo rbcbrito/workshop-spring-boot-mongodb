3fbced2de542cf95c6f7e170350b47a8b7db7d6f
Roberto Bruno <robertobcb@gmail.com>
2022-09-29 13:46:49 -0300

    Operacao de instanciacao da base de dados

--- src/main/java/com/rbtech/workshopmongo/resources/UserResource.java
+++ src/main/java/com/rbtech/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.rbtech.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,11 +10,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rbtech.workshopmongo.domain.User;
+import com.rbtech.workshopmongo.dto.UserDTO;
 import com.rbtech.workshopmongo.services.UserService;
 
-//Diz que a classe é um recurso Rest
 @RestController
-//Diz o caminho para endpoint
 @RequestMapping(value = "/users")
 public class UserResource {
 	
@@ -21,9 +21,10 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 }
