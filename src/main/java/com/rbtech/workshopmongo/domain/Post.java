2898b16e7fcc88b449afd5ba1e906c8bc3a5678f
Roberto Bruno <robertobcb@gmail.com>
2022-09-30 20:33:34 -0300

    Acrecentando comentarios aos posts

--- src/main/java/com/rbtech/workshopmongo/domain/Post.java
+++ src/main/java/com/rbtech/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.rbtech.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.rbtech.workshopmongo.dto.AuthorDTO;
+import com.rbtech.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,8 +21,10 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	
+
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -73,6 +78,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -89,7 +102,5 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 
 }
